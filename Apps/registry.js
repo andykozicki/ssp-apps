65d25280e6fcdf63a5dbf7bd5f143da6c63c20a9
andykozicki <andy.kozicki@sterlingspecialtyrx.com>
2026-03-05 10:27:02 -0600

    Add files via upload

---
+++ Apps/registry.js
@@ -0,0 +1,29 @@
+const appRegistry = [
+  {
+    "id": "rts-calculator",
+    "name": "RTS Calculator",
+    "description": "Generate allowable previous fill dates for controlled prescriptions.",
+    "folder": "rts-calculator",
+    "iconBg": "bg-green-50 dark:bg-green-900/30",
+    "iconColor": "text-green-600 dark:text-green-400",
+    "svg": "<path stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z\"></path>"
+  },
+  {
+    "id": "afrezza-calculator",
+    "name": "Afrezza Calculator",
+    "description": "Calculate day supply and calculate correction dose audit risk.",
+    "folder": "afrezza-calculator",
+    "iconBg": "bg-green-50 dark:bg-green-900/30",
+    "iconColor": "text-green-600 dark:text-green-400",
+    "svg": "<path stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"M9 7h6m0 10v-3m-3 3h.01M9 17h.01M9 14h.01M12 14h.01M15 11h.01M12 11h.01M9 11h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z\"></path>"
+  },
+    {
+    "id": "zen-snake",
+    "name": "Zen Snake",
+    "description": "Officially approved clinical stress reduction tool. Clear your mind while on hold.",
+    "folder": "zen-snake",
+    "iconBg": "bg-blue-50 dark:bg-blue-900/30",
+    "iconColor": "text-blue-500 dark:text-blue-400",
+    "svg": "<path stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z\"></path>"
+  }
+];
