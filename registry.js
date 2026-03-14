a9073692404ae8769cb50b33063045fe840f3b8b
andykozicki <andy.kozicki@sterlingspecialtyrx.com>
2026-03-13 20:38:52 -0500

    Update registry.js
    adding pomodoro timer, multi-calc, snake game logo

--- registry.js
+++ registry.js
@@ -9,21 +9,30 @@ const appRegistry = [
     "svg": "<path stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z\"></path>"
   },
   {
-    "id": "afrezza-calculator",
-    "name": "Afrezza Calculator",
-    "description": "Calculate day supply and calculate correction dose audit risk.",
-    "folder": "afrezza-calculator",
+    "id": "multi-calc",
+    "name": "Clinical Calculators",
+    "description": "Helpful Calculators for Afrezza, Tirosint & Eysuvis.",
+    "folder": "multi-calc",
     "iconBg": "bg-green-50 dark:bg-green-900/30",
     "iconColor": "text-green-600 dark:text-green-400",
     "svg": "<path stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"M9 7h6m0 10v-3m-3 3h.01M9 17h.01M9 14h.01M12 14h.01M15 11h.01M12 11h.01M9 11h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z\"></path>"
   },
-    {
+  {
     "id": "zen-snake",
     "name": "Zen Snake",
     "description": "Officially approved clinical stress reduction tool. Clear your mind while on hold.",
     "folder": "zen-snake",
     "iconBg": "bg-blue-50 dark:bg-blue-900/30",
     "iconColor": "text-blue-500 dark:text-blue-400",
-    "svg": "<path stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z\"></path>"
+    "svg": "<text y=\"20\" font-size=\"20\">🐍</text>"
+  },
+  {
+    "id": "pomodoro-timer",
+    "name": "Pomodoro Timer",
+    "description": "Time management tool using the Pomodoro technique to improve focus and productivity.",
+    "folder": "pomodoro-timer",
+    "iconBg": "bg-red-50 dark:bg-red-900/30",
+    "iconColor": "text-red-500 dark:text-red-400",
+    "svg": "<text y=\"20\" font-size=\"20\">🍅</text>"
   }
 ];
