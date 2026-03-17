ba390cfbd1bf036ddc8751dd93e72cc124ff25ba
andykozicki <andy.kozicki@sterlingspecialtyrx.com>
2026-03-17 11:29:09 -0500

    Remove Snake and Pomodoro
    For public facing dashboard

--- registry.js
+++ registry.js
@@ -17,22 +17,3 @@ const appRegistry = [
     "iconColor": "text-green-600 dark:text-green-400",
     "svg": "<path stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"M9 7h6m0 10v-3m-3 3h.01M9 17h.01M9 14h.01M12 14h.01M15 11h.01M12 11h.01M9 11h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z\"></path>"
   },
-  {
-    "id": "zen-snake",
-    "name": "Zen Snake",
-    "description": "Officially approved clinical stress reduction tool. Clear your mind while on hold.",
-    "folder": "zen-snake",
-    "iconBg": "bg-blue-50 dark:bg-blue-900/30",
-    "iconColor": "text-blue-500 dark:text-blue-400",
-    "svg": "<text y=\"20\" font-size=\"20\">🐍</text>"
-  },
-  {
-    "id": "pomodoro-timer",
-    "name": "Pomodoro Timer",
-    "description": "Time management tool using the Pomodoro technique to improve focus and productivity.",
-    "folder": "pomodoro-timer",
-    "iconBg": "bg-red-50 dark:bg-red-900/30",
-    "iconColor": "text-red-500 dark:text-red-400",
-    "svg": "<text y=\"20\" font-size=\"20\">🍅</text>"
-  }
-];
