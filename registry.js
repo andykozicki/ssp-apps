516acf9534898d30a038ff3d8a2dc0f1c65c23ba
andykozicki <andy.kozicki@sterlingspecialtyrx.com>
2026-03-17 15:59:30 -0500

    Update registry.js
    patched previous update
    added missing ];

--- registry.js
+++ registry.js
@@ -16,4 +16,5 @@ const appRegistry = [
     "iconBg": "bg-green-50 dark:bg-green-900/30",
     "iconColor": "text-green-600 dark:text-green-400",
     "svg": "<path stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"M9 7h6m0 10v-3m-3 3h.01M9 17h.01M9 14h.01M12 14h.01M15 11h.01M12 11h.01M9 11h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z\"></path>"
-  },
+  }
+];
